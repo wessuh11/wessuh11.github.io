a701da418050c07a735fde98fbfd379975059fb9
wessuh11 <greenblind11@naver.com>
2022-02-17 16:25:23 +0900

    오버플로우 확인 및 스크롤 바 위치 초기화 요망

--- script/script.js
+++ script/script.js
@@ -1,21 +1,34 @@
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'vertical',
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.next',
-      prevEl: '.prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
+$(function() {
+
+  const swiper = new Swiper('.swiper', {
+      // Optional parameters
+      direction: 'vertical',
+      loop: true,
+      
+      // If we need pagination
+      pagination: {
+        el: '.swiper-pagination',
+      },
+      
+      // Navigation arrows
+      navigation: {
+        nextEl: '.next',
+        prevEl: '.prev',
+      },
+      
+      
+      // And if we need scrollbar
+      scrollbar: {
+        el: '.swiper-scrollbar',
+      },
+      
+      autoHeight: true,
+      // freeMode: true,
+      // mousewheel: true,
+      // watchOverflow: false,
+    });
+
+  $('.summaries').click(function(){
+    $('.swiper-slide').scrolltop(0);
   });
+});
